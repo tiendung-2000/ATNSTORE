f0dd7f4c9ab005edd11857e61ca021324bfa9b31
Ngo Tien Dung <67892236+onlykrist@users.noreply.github.com>
2021-04-29 14:27:29 +0700

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(session({
 }));
 
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017';
+var url = 'mongodb+srv://ngotiendung:2012dung@cluster0.7kcqi.mongodb.net/test';
 
 var hbs = require('hbs')
 app.set('view engine', 'hbs')
@@ -193,5 +193,6 @@ app.post('/doupdate', async (req, res) => {
     }
 })
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
 console.log("Server is running 5000")
